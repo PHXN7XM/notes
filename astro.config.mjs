0851fb97beb511ae7d34e3f0ec54c3cac3b64dc4
PHXN7XM <elias.hernandez25ibi@nube.unadmexico.mx>
2026-02-10 20:13:45 -0600

    Base sitio astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      favicon: "/public/shion.ico",
+      title: "PHXNTXM",
+      logo: {
+        src: "/public/shion.ico",
+      },
+      customCss: ['./src/styles/custom.css'],
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/PHXN7XM",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Sistemas de estudio",
+          autogenerate: { directory: "sistemasestudio" },
+        },
+      ],
+    }),
+  ],
+
+  adapter: vercel(),
 });
